56576d9f06cca523a23bc7a2755cd0864323b517
Merve Tutar <mervetutar78@gmail.com>
2025-02-23 23:05:31 +0300

    Create users.py

---
+++ app/controllers/users.py
@@ -0,0 +1,27 @@
+from flask import session
+
+
+def UserLogin(username, password):
+    if username == "admin" and password == "admin":
+        session["username"] = username
+        return True
+    else:
+        return False
+
+def UserLogout():
+    if "username" in session:
+        del session["username"]
+        return True
+    else:
+        return False
+
+def GetCurrentUsername():
+    username = ""
+    loginAuth = False
+    if "username" in session:
+        username = session["username"]
+        loginAuth = True
+    return username, loginAuth
+
+
+
